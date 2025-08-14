906d95bf0ea98f746f9c4e393781c48d568deb36
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-08-14 19:36:03 +0200

    removes dependency on worker-timers, always use native timers

--- src/lib/get-timer.ts
+++ src/lib/get-timer.ts
@@ -1,40 +1,20 @@
-import { clearInterval as clearI, setInterval as setI } from 'worker-timers'
-import isBrowser, { isWebWorker, isReactNativeBrowser } from './is-browser'
 import type { TimerVariant } from './shared'
 
 // dont directly assign globals to class props otherwise this throws in web workers: Uncaught TypeError: Illegal invocation
 // See: https://stackoverflow.com/questions/9677985/uncaught-typeerror-illegal-invocation-in-chrome
 
 export interface Timer {
-	set: typeof setI
-	clear: typeof clearI
-}
-
-const workerTimer: Timer = {
-	set: setI,
-	clear: clearI,
+	set: typeof setInterval
+	clear: typeof clearInterval
 }
 
 const nativeTimer: Timer = {
-	set: (func, time) => setInterval(func, time),
-	clear: (timerId) => clearInterval(timerId),
+	set: setInterval,
+	clear: clearInterval,
 }
 
 const getTimer = (variant: TimerVariant): Timer => {
-	switch (variant) {
-		case 'native': {
-			return nativeTimer
-		}
-		case 'worker': {
-			return workerTimer
-		}
-		case 'auto':
-		default: {
-			return isBrowser && !isWebWorker && !isReactNativeBrowser
-				? workerTimer
-				: nativeTimer
-		}
-	}
+	return nativeTimer
 }
 
 export default getTimer
